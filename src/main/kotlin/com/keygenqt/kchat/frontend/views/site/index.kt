325e3f5ec657859dacc9d631fa79163c70588629
Vitaliy Zarubin <keygenqt@gmail.com>
2021-08-06 14:00:53 +0300

    Update readme

--- src/main/kotlin/com/keygenqt/kchat/frontend/views/site/index.kt
+++ src/main/kotlin/com/keygenqt/kchat/frontend/views/site/index.kt
@@ -43,6 +43,7 @@ fun HTML.index() {
                                 li {
                                     +"Template - "
                                     a {
+                                        target = "_blank"
                                         href = "https://github.com/Kotlin/kotlinx.html"
                                         +"kotlinx.html"
                                     }
